decb41b76f5286ff489d131501071f58a43dd806
Khalid Sookia <khalidsookia@gmail.com>
2015-06-22 18:12:16 +0200

    Creation of repo with base structure

---
+++ config/users.php
@@ -0,0 +1,22 @@
+<?php
+
+$users = [
+    [
+        "nom" => "Khalid",
+        "email" => "khalidsookia@gmail.com",
+        "salt" => "123456789",
+        "password" => "khalid"
+    ],
+    [
+        "nom" => "Aurelie",
+        "email" => "aurelie.birot@club.fr",
+        "salt" => "azerty",
+        "password" => "aurelie"
+    ],
+    [
+        "nom" => "Gilles",
+        "email" => "gilles.fourot@atos.net",
+        "salt" => "wxcv",
+        "password" => "gilles"
+    ]
+];
